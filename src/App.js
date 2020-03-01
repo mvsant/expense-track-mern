66d96c37d75cc90819bcce1db7b6daabff3d5a84
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-01 14:50:25 -0300

    Criado componente Balance e adicionado a App.js.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Balance from './components/Balance';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <Header />
       </header>
     </div>
+    <Balance/>
+    </>
   );
 }
 
