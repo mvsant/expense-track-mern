8dc49776da5ddc9de06d86daa0d7199b79a16ce8
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-01 17:01:35 -0300

    Refatorado o componente Transaction.js e adicionadas funcionalidades de formatação e stilo.

--- src/components/Transaction/index.js
+++ src/components/Transaction/index.js
@@ -3,10 +3,11 @@ import "./index.css";
 
 const Transaction = ({transaction}) => {
 
+  const sign = transaction.amount < 0 ? '-' : '+' ;
+
   return (
-    /* Transaction */
-    <li className="minus" /* id={transaction.id} */>
-    {transaction.text} <span>{transaction.amount}</span>
+    <li className={transaction.amount < 0 ? "minus":"plus"}>
+    {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
     <button className="delete-btn">X</button>
   </li>
   );
