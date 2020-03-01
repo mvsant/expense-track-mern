2c3df94198e94e459460894a4e2973362c529897
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-01 16:41:48 -0300

    Provido context para TransactionList.js.

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React , {useContext} from "react";
 import "./index.css";
+import {GlobalContext} from '../../context/GlobalState';
 
 const TransactionList = () => {
+  const {transactions} = useContext(GlobalContext);
+
+  console.log(transactions);
+
   return (
     <>
       <h3>History</h3>
       <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>-$400</span>
+        {transactions.map(transaction => (
+        <li className="minus" id={transaction.id}>
+          {transaction.text} <span>{transaction.amount}</span>
           <button className="delete-btn">x</button>
         </li>
+
+        ))}
       </ul>
     </>
   );
