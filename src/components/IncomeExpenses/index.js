d601403a4001f8fdf16bba13a4a50e464aee7b80
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-01 15:08:18 -0300

    Refatorado o componente IncomeExpenses para eliminação de warnings.

--- src/components/IncomeExpenses/index.js
+++ src/components/IncomeExpenses/index.js
@@ -4,14 +4,14 @@ import './index.css'
 
  const IncomeExpenses = () => {
     return (
-        <div class="inc-exp-container">
+        <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p id="money-plus" class="money plus">+$0.00</p>
+          <p id="money-plus" className="money plus">+$0.00</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p id="money-minus" class="money minus">-$0.00</p>
+          <p id="money-minus" className="money minus">-$0.00</p>
         </div>
       </div>
     )
