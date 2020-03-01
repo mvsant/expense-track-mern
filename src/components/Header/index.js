f7cb4c313d18ab8c7252cde417d3c15467286a29
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-01 14:38:52 -0300

    Criado componente Header.

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css'
+
+
+ const Header = () => {
+    return (
+        <h2>
+            Expense tracker
+        </h2>
+    )
+}
+
+export default Header;
