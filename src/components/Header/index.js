cf09d8527141e5a18e3cca9d78a6bfb6ecff8a46
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-03 13:48:56 -0300

    Refatorado o componente Header.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,14 @@
 import React, { useContext } from "react";
 import { GlobalContext } from "../../context/GlobalState";
 import { cTheme, textColor } from "../../context/GlobalTheme";
-import { button } from "./theme";
 import ThemeButton from '../ThemeButton';
 
 const Header = () => {
   const { theme } = useContext(GlobalContext);
-  const { changeTheme } = useContext(GlobalContext);
 
-  function handleTheme() {
-    return theme === "light" ? "dark" : "light";
-  }
   return (
     <>
       <h2 style={cTheme(theme, textColor)}>Expense tracker</h2>
-      <button style={cTheme(theme, button)}>Theme son</button>
-      <button onClick={() => changeTheme(handleTheme())}>Change theme</button>
       <ThemeButton/>
     </>
   );
