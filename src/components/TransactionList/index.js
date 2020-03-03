532099a524aa4e0d24eda9f2b5a162c2d0555be5
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-02 21:40:53 -0300

    Refatorado o componente TransactionList.

--- src/components/TransactionList/index.js
+++ src/components/TransactionList/index.js
@@ -1,15 +1,15 @@
 import React, { useContext } from "react";
-import "./index.css";
+import {list} from './theme';
 import Transaction from "../Transaction";
 import { GlobalContext } from "../../context/GlobalState";
 
 const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, theme } = useContext(GlobalContext);
 
   return (
     <>
       <h3>History</h3>
-      <ul className="list">
+      <ul style={list}>
         {transactions.map(transaction => (
           <Transaction key={transaction.id} transaction={transaction} />
         ))}
