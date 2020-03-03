d7e85cab46079e54b9f258a08f19863786af7340
Marlon Veiga <marlon_veiga@hotmail.com>
2020-03-03 19:36:14 -0300

    Iniciada composição das rotas e controllers.

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const morgan = require('morgan');
 
 dotenv.config({path: './config/config.env'});
 
+const transactions = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req,res) => res.send('Hello'));
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
